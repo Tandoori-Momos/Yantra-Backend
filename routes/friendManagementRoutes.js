bd6b2c69123372db543d550b06e1a96318a517ac
sai-sreekhar <sai.sreekhar@gmail.com>
2023-06-07 21:18:34 +0530

    feat: modified routes

--- routes/friendManagementRoutes.js
+++ routes/friendManagementRoutes.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const friendManagementController = require("../controllers/friendManagement/friendManagementController");
+const auth = require("../middleware/authMiddleware");
 const friendManagementRouter = express.Router();
 
-friendManagementRouter.route("/").post(friendManagementController.addFriend);
-friendManagementRouter.route("/").get(friendManagementController.getFriends);
 friendManagementRouter
-  .route("/:friendId")
-  .delete(friendManagementController.removeFriend);
-  
+  .route("/")
+  .post(auth, friendManagementController.sendFriendRequest);
+friendManagementRouter
+  .route("/accept")
+  .post(auth, friendManagementController.acceptFriendRequest);
+friendManagementRouter
+  .route("/reject")
+  .post(auth, friendManagementController.rejectFriendRequest);
+
+
+//on hold
+// friendManagementRouter.route("/cancel").post(auth,friendManagementController.cancelFriendRequest);
+// friendManagementRouter.route("/").get(friendManagementController.getFriends);
+// friendManagementRouter
+//   .route("/:friendId")
+//   .delete(friendManagementController.removeFriend);
+
 module.exports = friendManagementRouter;
