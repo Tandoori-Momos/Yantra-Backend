79591d00657bf390ffdbd2898d19ce596dc117d7
sai-sreekhar <sai.sreekhar@gmail.com>
2023-06-07 20:02:17 +0530

    feat: added route to get profile

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -2,7 +2,10 @@ const userModel = require("../../models/userModel");
 const AppError = require("../../utils/appError");
 const catchAsync = require("../../utils/catchAsync");
 const { errorCodes, loginType } = require("../../utils/constants");
-const { registerUserBodyValidation } = require("./validationSchema");
+const {
+  registerUserBodyValidation,
+  loginUserBodyValidation,
+} = require("./validationSchema");
 const bcrypt = require("bcrypt");
 const { generateTokens } = require("./utils");
 
@@ -52,6 +55,67 @@ exports.registerUser = catchAsync(async (req, res, next) => {
   res.status(201).json({
     status: "success",
     message: "User registered successfully",
-    accessToken
+    accessToken,
+  });
+});
+
+exports.loginUser = catchAsync(async (req, res, next) => {
+  const { error } = loginUserBodyValidation(req.body);
+  if (error) {
+    return next(
+      new AppError(
+        error.details[0].message,
+        400,
+        errorCodes.INPUT_PARAMS_INVALID
+      )
+    );
+  }
+
+  const user = await userModel.findOne({ username: req.body.username });
+  if (!user) {
+    return next(
+      new AppError(
+        "Invalid username or password",
+        401,
+        errorCodes.INVALID_USERNAME_OR_PASSWORD
+      )
+    );
+  }
+
+  const verifiedPassword = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+
+  if (!verifiedPassword) {
+    return next(
+      new AppError(
+        "Invalid username or password",
+        401,
+        errorCodes.INVALID_USERNAME_OR_PASSWORD
+      )
+    );
+  }
+
+  const { accessToken } = await generateTokens(user);
+
+  res.status(200).json({
+    message: "Logged in sucessfully",
+    accessToken,
+  });
+});
+
+exports.getUserProfile = catchAsync(async (req, res, next) => {
+  const user = await userModel.findById(req.user._id);
+  if (!user) {
+    return next(
+      new AppError("User not found", 404, errorCodes.USER_NOT_FOUND)
+    );
+  }
+
+  res.status(200).json({
+    status: "success",
+    message: "User profile fetched successfully",
+    user,
   });
 });
--- controllers/user/validationSchema.js
+++ controllers/user/validationSchema.js
@@ -3,16 +3,23 @@ const Joi = require("joi");
 module.exports = {
   registerUserBodyValidation: (body) => {
     const schema = Joi.object({
-        username: Joi.string().required(),
-        password: Joi.string().required(),
-        email: Joi.string().required(),
-        firstName: Joi.string().required(),
-        lastName: Joi.string().required(),
-        mobileNumber: Joi.string().required(),
-        age: Joi.number().required(),
-        location: Joi.string().required(),
-        interests: Joi.array().items(Joi.string()).required(),
-        healthConditions: Joi.array().items(Joi.string()).required(),
+      username: Joi.string().required(),
+      password: Joi.string().required(),
+      email: Joi.string().required(),
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      mobileNumber: Joi.string().required(),
+      age: Joi.number().required(),
+      location: Joi.string().required(),
+      interests: Joi.array().items(Joi.string()).required(),
+      healthConditions: Joi.array().items(Joi.string()).required(),
+    });
+    return schema.validate(body);
+  },
+  loginUserBodyValidation: (body) => {
+    const schema = Joi.object({
+      username: Joi.string().required(),
+      password: Joi.string().required(),
     });
     return schema.validate(body);
   },
