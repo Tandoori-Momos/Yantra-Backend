5074198f003a307d46bb27d935520af2e724aa09
sai-sreekhar <sai.sreekhar@gmail.com>
2023-06-07 20:13:02 +0530

    feat: added new constants

--- utils/constants.js
+++ utils/constants.js
@@ -10,6 +10,9 @@ const errorCodes = {
   UNKNOWN_ERROR: 0,
   EXCEPTION: 1,
   INVALID_URL: 2,
+  INPUT_PARAMS_INVALID: 3,
+  INVALID_USERNAME_OR_PASSWORD: 4,
+  USER_NOT_FOUND: 5
 };
 
 module.exports = {
